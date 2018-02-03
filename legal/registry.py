4d62ee6befbbcc5750bc54176e94f4c73eddfaa6
grilo <joao.grilo@gmail.com>
2018-02-03 02:42:02 +0100

    Use the official SPDX registry.
    Be a good citizen by caching results for 6 months.

--- legal/registry.py
+++ legal/registry.py
@@ -1,20 +1,78 @@
-# Note: these are compiled with re.DOTALL
+#!/usr/bin/env python
 
-# See:
-# https://github.com/spdx/license-list-data/blob/master/json/licenses.json
-# https://github.com/spdx/license-list-data/tree/master/text
+import logging
+import os
+import json
+import time
+import multiprocessing as mp
 
-licenses = {
-    'MIT': {
-        'short': r'MIT',
-        'long': r'.*MIT.*License.*Permission is hereby.*',
-    },
-    'APL': {
-        'short': r'Apache-2.0',
-        'long': r'(Apache License|The Apache)',
-    },
-    'UNLICENSE': {
-        'short': r'Unlicense',
-        'long': r'Unlicense',
-    },
-}
+import requests
+
+
+class SPDX(object):
+    """Returns all SPDX licenses, built-in cache mechanism.
+
+    Handles like an immutable dictionary.
+    """
+
+    @staticmethod
+    def _download_all(url):
+        lics = {}
+        results = requests.get(url).json()
+        details = [lic['detailsUrl'] for lic in results['licenses']]
+
+        logging.info("Downloading (%s) licenses from: %s", len(details), url)
+
+        pool = mp.Pool()
+        for response in pool.imap_unordered(requests.get, details):
+            license = response.json()
+            logging.debug("Downloaded: %s", license['licenseId'])
+            lics[license['licenseId']] = license
+        pool.close()
+        pool.join()
+
+        return lics
+
+
+    def __init__(self, spdx_url='https://spdx.org/licenses', cache_dir=None):
+        self.spdx_url = spdx_url
+        if cache_dir is None:
+            cache_dir = os.path.join(os.environ.get("HOME", os.getcwd()), '.licscan')
+        self.cache_dir = cache_dir
+        self.cache_ttl = 15811200  # Seconds
+        self._licenses = {}
+
+    @property
+    def licenses(self):
+        """Layered approach for handling the cache:
+        1. prefer local disk.
+        2. if not found or expired, download it
+        """
+
+        lic_idx = os.path.join(self.cache_dir, 'licenses.json')
+
+        # If local cache is invalid, rebuild it
+        if not os.path.isfile(lic_idx) or \
+            time.time() - os.path.getmtime(lic_idx) > self.cache_ttl:
+            logging.warning("Cache invalid, downloading...")
+
+            if not os.path.isdir(self.cache_dir):
+                os.makedirs(self.cache_dir)
+
+            lock = os.path.join(self.cache_dir, '.updating')
+            if os.path.isfile(lock):
+                return self._licenses
+            open(lock).close()
+            self._licenses = self._download_all(os.path.join(self.spdx_url, 'licenses.json'))
+            os.remove(lock)
+
+            with open(lic_idx, 'w') as lics:
+                logging.info("Caching results...")
+                json.dump(self._licenses, lics)
+
+        # Return locally stored licenses
+        if not self._licenses:
+            logging.info("Retrieving cached licenses...")
+            self._licenses = json.loads(open(lic_idx, 'r').read())
+
+        return self._licenses
