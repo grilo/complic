5e4292a1f0162e312edfd290ff47459ea47aed23
grilo <joao.grilo@gmail.com>
2018-02-13 01:57:18 +0100

    Remove tfidf, feature not currently needed.

--- legal/base.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-
-class Application(object):
-
-    def __init__(self, path, licenses, identifier=None):
-        self.path = path
-        self.licenses = licenses
-        self.identifier = identifier or path
