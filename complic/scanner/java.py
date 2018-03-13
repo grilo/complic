09839f815620593e11a95ed579595db84915f819
Joao Grilo <joao.grilo@gmail.com>
2018-03-14 00:33:02 +0100

    Add test coverage to java scanner.

--- complic/scanner/java.py
+++ complic/scanner/java.py
@@ -6,8 +6,7 @@
 import logging
 import re
 import os
-import subprocess
-import shlex
+import distutils.spawn
 
 import complic.utils.fs
 import complic.utils.shell
@@ -25,40 +24,21 @@ class Scanner(base.Scanner):
     def __init__(self):
         super(Scanner, self).__init__()
 
-        self.register_handler(re.compile(r'.*/pom.xml$'),
-                              Scanner.handle_pom)
-
-    @staticmethod
-    def handle_pom(file_path):
-        """The licensing plugin which does all the hard work for us.
-
-        This is not very elegant since, in the case of multi-module projects,
-        we're running too many times without any need."""
-
-        logging.debug("Matched pom handler: %s", file_path)
-
-        command = "mvn org.codehaus.mojo:license-maven-plugin:1.13"
-        command += ":add-third-party -q -B -f %s" % (file_path)
-        logging.info("Running license-mvn-plugin on: %s", file_path)
-        rc, out, err = complic.utils.shell.cmd(command)
-        if rc != 0:
-            return []
-
-        deps = []
-        for path in complic.utils.fs.Find(os.path.dirname(file_path)).files:
-            if path.endswith('THIRD-PARTY.txt'):
-                deps.extend(Scanner.parse_thirdparty(path))
-        return deps
+        if distutils.spawn.find_executable('mvn'):
+            self.register_handler(re.compile(r'.*/pom.xml$'),
+                                  Scanner.handle_pom)
+        else:
+            logging.error("Unable to find 'mvn' executable in PATH.")
 
     @staticmethod
     def parse_thirdparty(thirdparty):
-        """Parse the THIRD-PARTY files.
+        """Parse the strings from THIRD-PARTY files.
 
         These probably aren't meant to be parsed directly, but the structure
         is regular enough for our use case."""
         regex = re.compile(r'\s(\(.*\)) (\w+.*) (\(.*\))')
-        dependencies = []
-        for line in open(thirdparty, 'r').read().splitlines():
+        dependencies = {}
+        for line in thirdparty.splitlines():
 
             if not line or line.startswith('List'):
                 continue
@@ -72,11 +52,40 @@ class Scanner(base.Scanner):
             coords = coords.replace('(', '')
             url = url.replace(')', '')
 
-            dependency = base.Dependency(**{'path': thirdparty})
-            dependency.identifier = 'java:' + coords
-            dependency.licenses.add(license_string)
-
-            dependencies.append(dependency)
+            identifier = 'java:' + coords
+            if not identifier in dependencies:
+                dependencies[identifier] = set()
+            dependencies[identifier].add(license_string)
 
         logging.debug("Dependencies found: %i", len(dependencies))
         return dependencies
+
+    @staticmethod
+    def handle_pom(file_path):
+        """The licensing plugin which does all the hard work for us.
+
+        This is not very elegant since, in the case of multi-module projects,
+        we're running too many times without any need."""
+
+        logging.debug("Matched pom handler: %s", file_path)
+
+        command = "mvn org.codehaus.mojo:license-maven-plugin:1.13"
+        command += ":add-third-party -q -B -f %s" % (file_path)
+        logging.info("Running license-mvn-plugin on: %s", file_path)
+        return_code, _, _ = complic.utils.shell.cmd(command)
+        if return_code != 0:
+            return []
+
+        deps = []
+        for path in complic.utils.fs.Find(os.path.dirname(file_path)).files:
+            if not path.endswith('THIRD-PARTY.txt'):
+                continue
+            string = open(path, 'r').read()
+            for identifier, licenses in Scanner.parse_thirdparty(string).items():
+
+                dependency = base.Dependency(**{'path': path})
+                dependency.identifier = identifier
+                dependency.licenses = licenses
+                deps.append(dependency)
+
+        return deps
---
+++ tests/test_scanner_java.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+import distutils
+
+import pytest
+
+#import complic.scanner.cocoapods
+from complic.scanner.java import Scanner
+from complic.utils import shell
+from complic.utils import fs
+
+
+def test_no_matches_if_not_in_path(mocker):
+    mocker.patch.object(distutils.spawn, 'find_executable')
+    distutils.spawn.find_executable.return_value = False
+
+    mocker.patch.object(Scanner, 'handle_pom')
+
+    s = Scanner()
+    s.scan([
+        'hello/pom.xml',
+        'world/anotherfile',
+    ])
+    Scanner.handle_pom.no_calls()
+
+
+def test_matches_pomfiles(mocker):
+    mocker.patch.object(distutils.spawn, 'find_executable')
+    distutils.spawn.find_executable.return_value = True
+
+    mocker.patch.object(Scanner, 'handle_pom')
+    Scanner.handle_pom.return_value = []
+    s = Scanner()
+    s.scan([
+        'hello/pom.xml',
+        'world/anotherfile',
+    ])
+    Scanner.handle_pom.assert_called_once_with('hello/pom.xml')
+
+def test_parse_thirdparty():
+    thirdparty = """Lists of 11 third-party dependencies.
+     (Unknown license) antlr (antlr:antlr:2.7.2 - no url defined)
+     (Unknown license) commons-beanutils (commons:beanutils:1.7.0 - no url defined)
+     (GPLv2) Commons Chain (commons:chain:1.1 - http://jakarta/${pom.artifactId.substring(8)}/)
+     (MIT) Commons Chain (commons:chain:1.1 - http://jakarta/${pom.artifactId.substring(8)}/)
+     (Common Public License Version 1.0) JUnit (junit:junit:3.8.1 - http://junit.org)
+    """
+
+    out = Scanner.parse_thirdparty(thirdparty)
+    assert len(out) == 4
+    assert 'java:antlr:antlr:2.7.2' in out
+    assert 'java:commons:beanutils:1.7.0' in out
+    assert 'java:commons:chain:1.1' in out
+    assert 'java:junit:junit:3.8.1' in out
+    assert 'Unknown license' in out['java:antlr:antlr:2.7.2']
+    assert 'GPLv2' in out['java:commons:chain:1.1']
+    assert 'MIT' in out['java:commons:chain:1.1']
+
+
+def test_handle_bad_pom(mocker):
+    mocker.patch.object(shell, 'cmd')
+    shell.cmd.return_value = (1, '', '')
+    assert not Scanner.handle_pom('fake_path')
+
+def test_handle_good_pom(tmpdir, mocker):
+
+    mocker.patch.object(shell, 'cmd')
+    shell.cmd.return_value = (0, '', '')
+
+    p = tmpdir.mkdir("project").join("THIRD-PARTY.txt")
+    p.write('fake')
+    thirdparty_txt = str(p.realpath())
+
+    mocker.patch.object(fs, 'Find')
+    fs.Find.return_value.files = ['/absolute/path', thirdparty_txt]
+
+    mocker.patch.object(Scanner, 'parse_thirdparty')
+    Scanner.parse_thirdparty.return_value = {
+        'java:commons:chain:1.1': set(['GPLv2', 'MIT']),
+        'java:antlr:antlr:2.7.2': set(['Uknown license']),
+    }
+
+    dependencies = Scanner.handle_pom('fake')
+
+    assert len(dependencies) == 2
+    assert dependencies[0].identifier == 'java:antlr:antlr:2.7.2'
+    assert dependencies[1].identifier == 'java:commons:chain:1.1'
+    assert len(dependencies[0].licenses) == 1
+    assert len(dependencies[1].licenses) == 2
